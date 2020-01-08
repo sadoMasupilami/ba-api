7f96228091ecd693c523580cc261d800a6a8fee2
Michael Klug <michiklug85@gmail.com>
2020-01-08 14:19:46 +0100

    added sql init script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+use ba;
+
+INSERT INTO person (first_name, last_name) VALUES
+('Michael', 'Klug'),
+('Hans', 'Moser');
+
+INSERT INTO cat (name, color, weight, person_id) VALUES
+('Fifo', 'tabby', 6.9, 1),
+('Garfield', 'orange', 9.7, 1),
+('Sepp', 'black', 4.7, 2);
