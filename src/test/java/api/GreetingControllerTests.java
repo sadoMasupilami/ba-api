2c2f19c0e2618753b91007492b5654372846da17
Michael Klug <michiklug85@gmail.com>
2020-05-30 11:06:06 +0200

    testing test

--- src/test/java/api/GreetingControllerTests.java
+++ src/test/java/api/GreetingControllerTests.java
@@ -1,31 +1,31 @@
-//package api;
-//
-//import org.junit.Assert;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.concurrent.TimeUnit;
-//
-//public class GreetingControllerTests {
-//
-//	@Test
-//	public void test1() {
-//
-//		Assert.assertTrue(true);
-//	}
-//
-//	@Test
-//	public void test2() {
-//
-//		Assert.assertTrue(true);
-//	}
-//
-//	@Test
-//	public void test3() {
-//		try {
-//			TimeUnit.SECONDS.sleep(3);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//		Assert.assertTrue(true);
-//	}
-//}
+package api;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class GreetingControllerTests {
+
+	@Test
+	public void test1() {
+
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void test2() {
+
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void test3() {
+		try {
+			TimeUnit.SECONDS.sleep(3);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Assert.assertTrue(true);
+	}
+}
